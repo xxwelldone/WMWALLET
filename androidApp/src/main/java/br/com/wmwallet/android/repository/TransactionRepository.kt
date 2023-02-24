d69cba4f763b9d477b5a5287604c57c89c5f987d
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2023-02-24 19:54:24 -0300

    Update

--- androidApp/src/main/java/br/com/wmwallet/android/repository/TransactionRepository.kt
+++ androidApp/src/main/java/br/com/wmwallet/android/repository/TransactionRepository.kt
@@ -3,18 +3,20 @@ package br.com.wmwallet.android.repository
 import br.com.wmwallet.api.Api
 
 import br.com.wmwallet.model.Transaction
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class TransactionRepository(private val api: Api = Api.instance) {
 
     suspend fun getTrans() = flow<List<Transaction>> {
         val chamada = api.getAll().transaction
         emit(chamada)
-    }
+    }.flowOn(Dispatchers.Default)
 
     suspend fun getProfile() = flow {
         emit(api.profile())
-    }
+    }.flowOn(Dispatchers.Default)
 
     companion object {
         val instance by lazy { TransactionRepository() }
